db4da0e1f89a96acf4d7843cf30b9e76fd056094
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-01-11 16:44:12 +0100

    Updated dotfiles

---
+++ lib/.config/.zsh/aliases.sh
@@ -0,0 +1,22 @@
+alias c='clear'
+alias cd..='cd ..'
+alias chgrp='chgrp --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chown='chown --preserve-root'
+alias l='ls -CF'
+alias la='ls -la'
+alias ll='ls -alF'
+alias nvim='neovim-qt'
+alias ping='ping -c 5'
+alias ports='netstat -tulanp'
+alias untar='tar -zxvf'
+alias v='vim'
+alias wget='wget -c'
+alias yt="youtube-dl --add-metadata -i -c -w -o '$HOME/Videos/youtube/%(title)s.%(ext)s' -f best"
+alias yta="youtube-dl --add-metadata -i -c -w -o '$HOME/Music/%(title)s.%(ext)s' -f best -x --audio-format mp3"
+alias play="mpg123"
+alias wow="~/.local/share/lutris/runners/wine/lutris-4.16-x86_64/bin/wine64 ~/Games/world-of-warcraft/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Wow.exe"
+alias pacman="sudo pacman"
+alias psqlopen="sudo su postgres -c psql"
+alias i3lock="i3lock-fancy"
+
---
+++ lib/.config/.zsh/defaults.sh
@@ -0,0 +1,3 @@
+export MANPATH="/usr/local/man:$MANPATH"
+export EDITOR="vim"
+export BROWSER="brave"

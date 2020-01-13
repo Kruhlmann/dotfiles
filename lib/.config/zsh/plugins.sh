d869302e34028537816f8a506abf5307a351a7ed
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-01-13 17:23:59 +0100

    Updated dotfiles

--- lib/.config/zsh/plugins.sh
+++ lib/.config/zsh/plugins.sh
@@ -1,1 +1,19 @@
-/home/ges/Documents/src/dotfiles/lib/.config/.zsh/plugins.sh
+plugins=(
+    catimg
+    docker
+    extract
+    gem
+    gitfast
+    last-working-dir
+    npm
+    pip
+    ssh-agent
+    sudo
+    zsh-autosuggestions
+    zsh-completions
+    zsh-syntax-highlighting
+)
+
+zstyle :omz:plugins:ssh-agent agent-forwarding on
+zstyle :omz:plugins:ssh-agent identities t420
+zstyle :omz:plugins:ssh-agent lifetime

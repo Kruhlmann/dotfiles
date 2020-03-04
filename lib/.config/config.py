925a16b313bc37b7e63332a6a8d258fbb0db8c18
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-03-05 00:54:57 +0100

    Updated dotfiles

---
+++ lib/.config/config.py
@@ -0,0 +1,23 @@
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
+# UA
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://accounts.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://docs.google.com/*')
+
+# Flags
+config.set('content.images', True, 'chrome-devtools://*')
+config.set('content.images', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+config.set('content.javascript.enabled', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+config.set('content.javascript.enabled', True, 'qute://*/*')
+
+# Bindings
+config.bind("yp", "spawn mpv --force-window yes {url}")
+config.bind("yh", "hint links spawn mpv --force-window yes {hint-url}")
+
+# Layout
+c.tabs.position = "top"
+
+# Behavior
+c.auto_save.session = True

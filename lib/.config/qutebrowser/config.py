eff84ed51973f85d049f4203ac0f7d5642b80ffe
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-03-05 19:47:26 +0100

    Updated dotfiles

--- lib/.config/qutebrowser/config.py
+++ lib/.config/qutebrowser/config.py
@@ -13,6 +13,9 @@ c_fg_blue = "#83a598"
 c_bg_purple = "#b16286"
 c_fg_purple = "#d3869b"
 
+# Aliases
+c.aliases["tor-on"] = "set content.proxy socks://localhost:9050/"
+
 # UA
 config.set("content.headers.user_agent", "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}", "https://web.whatsapp.com/")
 config.set("content.headers.user_agent", "Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0", "https://accounts.google.com/*")
@@ -36,6 +39,7 @@ config.bind("<Ctrl-i>", "inspector")
 config.bind(".", "tab-next")
 config.bind(",", "tab-prev")
 config.bind("<Ctrl-Shift-p>", "spawn --userscript ~/.scripts/qute-bitwarden")
+config.bind(" z", "spawn --userscript ~/.scripts/qute-goyo")
 # config.bind(""", "enter-mode jump_mark")
 # config.bind("+", "zoom-in")
 # config.bind("-", "zoom-out")

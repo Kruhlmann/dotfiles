d9e7717e1072bf92a102db4a656138a30e5e8d66
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-03-18 19:34:56 +0100

    Updated dotfiles

--- lib/.config/qutebrowser/imports/content.py
+++ lib/.config/qutebrowser/imports/content.py
@@ -13,6 +13,6 @@ def apply(config, c):
     c.content.local_content_can_access_remote_urls = False
     c.content.local_storage = True
     c.content.pdfjs = True
-    c.content.proxy = None
+    c.content.proxy = "none"
     c.content.default_encoding = "utf-8"
     c.content.geolocation = False

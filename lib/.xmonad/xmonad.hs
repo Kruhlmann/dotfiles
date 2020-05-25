84ee398f094dbcbd9e50c98287c70d02b7baf287
Andreas Stigsbøl Krühlmann <andreas@kruhlmann.dev>
2020-05-25 08:55:06 +0200

    Merge

--- lib/.xmonad/xmonad.hs
+++ lib/.xmonad/xmonad.hs
@@ -43,6 +43,17 @@ import XMonad.Layout.ToggleLayouts          -- Full window at any time
 import XMonad.Layout.BinarySpacePartition
 import XMonad.Layout.Mosaic
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.FixedColumn
+import XMonad.Layout.Grid
+import XMonad.Layout.IM
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Reflect
+import XMonad.Layout.SimpleFloat
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.WindowArranger
 import qualified DBus as D
 import qualified DBus.Client as D
 import qualified Codec.Binary.UTF8.String as UTF8
@@ -56,7 +67,16 @@ c_white = "#eeeeee"
 c_green = "#98971a"
 c_yellow_alt = "#fabd2f"
 
+-- Layout string.
+curLayout :: X String
+curLayout = gets windowset >>= return . description . W.layout . W.workspace . W.current
+
 myTerminal = "termite"
+
+-- Spawn vars.
+spRofi = "rofi -modi drun -show drun -display-drun 'Run'"
+spMaim = "maim -sl -u -c 0.2,0.4,1.0,0.7 -b 0 | xclip -selection clipboard -t image/png"
+
 ---- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 myWorkspaces :: [String]
@@ -64,20 +84,23 @@ myWorkspaces = ["1:\xfa9e", "2: \xe7c5", "3: \xfb6e", "4: \xf0c0", "5: \xf023", 
 
 binds conf@(XConfig {XMonad.modMask = modm}) = M.fromList $ [
     ((modm,               xK_Return), spawn $ XMonad.terminal conf),
-    ((modm,               xK_d     ), spawn "rofi -modi drun -show drun -display-drun 'Run'"),
+    ((modm,               xK_d     ), spawn spRofi),
     ((modm,               xK_q     ), kill),
-    ((modm .|. shiftMask, xK_s     ), spawn "maim -sl -u -c 0.2,0.4,1.0,0.7 -b 0 | xclip -selection clipboard -t image/png"),
+    ((modm .|. shiftMask, xK_s     ), spawn spMaim),
     ((modm .|. shiftMask, xK_l     ), spawn "xscreensaver-command --lock"),
-    ((modm,               xK_Tab   ), sendMessage NextLayout),
+    ((modm,               xK_Tab   ), sendMessage NextLayout >> (curLayout >>= \d->spawn $"notify-send "++d)),
+    ((modm,               xK_f     ), sendMessage ToggleLayout),
     ((modm .|. shiftMask, xK_o     ), setLayout $ XMonad.layoutHook conf),
     ((modm,               xK_j     ), windows W.focusDown),
-    ((modm,               xK_k     ), windows W.focusUp  ),
-    ((modm,               xK_i     ), windows W.focusMaster  ),
-    ((modm .|. shiftMask, xK_j     ), windows W.swapDown  ),
-    ((modm .|. shiftMask, xK_k     ), windows W.swapUp    ),
+    ((modm,               xK_k     ), windows W.focusUp),
+    ((modm .|. shiftMask, xK_j     ), windows W.swapDown),
+    ((modm .|. shiftMask, xK_k     ), windows W.swapUp),
+    ((modm              , xK_i     ), sendMessage (IncMasterN 1)),
+    ((modm              , xK_o     ), sendMessage (IncMasterN (-1))),
     ((modm,               xK_h     ), sendMessage Shrink),
     ((modm,               xK_l     ), sendMessage Expand),
     ((modm .|. shiftMask, xK_space ), withFocused $ windows . W.sink),
+    ((modm,               xK_r     ), refresh),
     ((modm              , xK_u      ), spawn "termite --name floatterm -e fzmp"),
     ((modm              , xK_g      ), spawn "termite --name floatterm -e lazygit"),
     ((modm              , xK_e      ), spawn "termite --name floatterm -e ranger"),
@@ -90,15 +113,17 @@ binds conf@(XConfig {XMonad.modMask = modm}) = M.fromList $ [
     ((modm              , xK_comma  ), spawn "mpc prev"),
     ((modm              , xK_p      ), spawn "mpc toggle"),
     ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)),
+    ((modm,               xK_Down),  nextWS),
+    ((modm,               xK_Up),    prevWS),
+    ((modm .|. shiftMask, xK_Right), shiftNextScreen),
+    ((modm .|. shiftMask, xK_Left),  shiftPrevScreen),
     ((modm              , xK_F2     ), spawn "xmonad --recompile; xmonad --restart")
-    --, ((modm,               xK_n     ), refresh)
-    --, ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-    --, ((modm              , xK_period), sendMessage (IncMasterN (-1)))
     --, ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     ] ++ [((m .|. modm, k), windows $ f i)
     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    --, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
     -- ++ [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
     -- | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
@@ -126,7 +151,9 @@ myManageHook = composeAll [
     title     =? "Firefox Preferences" --> doFloat,
     title     =? "Session Manager - Mozilla Firefox" --> doFloat,
     title     =? "Firefox Add-on Updates" --> doFloat,
-    title     =? "Clear Private Data" --> doFloat
+    title     =? "Clear Private Data" --> doFloat,
+    -- Fullscreen.
+    isFullscreen --> doFullFloat
     ]
 
 -- Mouse bindings
@@ -142,22 +169,49 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
                                        >> windows W.shiftMaster))
     ]
 
-myLayoutHook = avoidStruts (
-       toggleLayouts Full (ThreeColMid 1 (1/20) (1/2)) ||| simpleTabbed ||| toggleLayouts Full (tiled))
-        where
-    -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
- 
-    -- The default number of windows in the master pane
-    nmaster = 1
- 
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
- 
-    -- Percent of screen to increment by when resizing panes
+-- A standard tiled layout, with a master pane and a secondary pane off to
+-- the side.  The master pane typically holds one window; the secondary
+-- pane holds the rest.  Copied from standard xmonad.hs template config.
+tiledLayout = Tall nmaster delta ratio
+  where
+    nmaster = 1      -- The default number of windows in the master pane.
+    ratio   = 1/2    -- Default proportion of screen occupied by master pane.
+    delta   = 3/100  -- Percent of screen to increment by when resizing panes.
 
-delta = 3/100 
-----Main Function
+-- Per-workspace layouts.  We set up custom layouts (and even lists of
+-- custom layouts) for the most important workspaces, so we can tweak
+-- window management fairly precisely.
+--
+-- Inspired by:
+--   http://kitenet.net/~joey/blog/entry/xmonad_layouts_for_netbooks/
+workspaceLayouts =
+  onWorkspace "1:\xfa9e" webLayouts $
+  onWorkspace "2: \xe7c5" codeLayouts $
+  onWorkspace "3: \xfb6e" imLayouts $
+  defaultLayouts
+  where
+    -- Combinations of our available layouts, which we can cycle through
+    -- using mod-Space.  'Mirror' applies a 90-degree rotation to a layout.
+    codeLayouts = fixedLayout ||| tiledLayout
+    webLayouts = tiledLayout
+    imLayouts = simpleTabbed
+    defaultLayouts = tiledLayout ||| Mirror tiledLayout ||| fixedLayout |||
+                     floatLayout ||| simpleTabbed
+
+    -- An 80-column fixed layout for Emacs and terminals.  The master
+    -- pane will resize so that the contained window is 80 columns.
+    fixedLayout = FixedColumn 1 20 80 10
+
+    -- A simple floating-window layout.  This isn't particularly good,
+    -- to be honest, but further configuration might improve it.
+    floatLayout = windowArrange simpleFloat
+
+-- Hook up my layouts.  We apply 'toggleLayouts' so that we can switch any
+-- window into or out of full-screen mode with a single command (see
+-- below).  We use 'smartBorders' to turn off the border around the focused
+-- window if it's the only window on the screen.  And 'avoidStruts' leaves
+-- space for gnome-panel and xmobar.
+myLayout = avoidStruts $ smartBorders $ toggleLayouts Full workspaceLayouts
 
 main = do
   forM_ [".xmonad-workspace-log", ".xmonad-title-log"] $ \file -> do
@@ -184,7 +238,7 @@ defaults = def{
     , terminal = myTerminal
     , workspaces = myWorkspaces
     , keys = binds
-    , layoutHook = smartBorders $ myLayoutHook
+    , layoutHook = smartBorders $ myLayout
     , focusedBorderColor = "red"
     , normalBorderColor = c_gray_alt
     , mouseBindings = myMouseBindings                           

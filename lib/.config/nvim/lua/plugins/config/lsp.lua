3362fd889937008c2529fa524d4f4456a94a8990
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-09-09 16:54:23 +0200

    Updated dotfiles

--- lib/.config/nvim/lua/plugins/config/lsp.lua
+++ lib/.config/nvim/lua/plugins/config/lsp.lua
@@ -24,8 +24,7 @@ local sources = {
 null_ls.config({sources = sources})
 
 local saga = require("lspsaga")
-local cmp = require('cmp')
--- setup nvim-cmp
+local cmp = require("cmp")
 
 saga.init_lsp_saga()
 protocol.make_client_capabilities()
@@ -102,53 +101,38 @@ local on_attach = function(client, bufnr)
 end
 
 local on_attach_ts = function(client)
-    -- disable tsserver formatting if you plan on formatting via null-ls
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
 
     local ts_utils = require("nvim-lsp-ts-utils")
-
-    -- defaults
     ts_utils.setup {
         debug = false,
         disable_commands = false,
         enable_import_on_completion = true,
-
-        -- import all
-        import_all_timeout = 5000, -- ms
+        import_all_timeout = 5000,
         import_all_priorities = {
-            buffers = 4, -- loaded buffer names
-            buffer_content = 3, -- loaded buffer content
-            local_files = 2, -- git files or files with relative path markers
-            same_file = 1 -- add to existing import statement
+            buffers = 4,
+            buffer_content = 3,
+            local_files = 2,
+            same_file = 1
         },
         import_all_scan_buffers = 100,
         import_all_select_source = false,
-
-        -- eslint
         eslint_enable_code_actions = true,
         eslint_enable_disable_comments = true,
         eslint_bin = "eslint_d",
         eslint_config_fallback = nil,
         eslint_enable_diagnostics = true,
         eslint_show_rule_id = true,
-
-        -- formatting
         enable_formatting = true,
         formatter = "prettier_d_slim",
         formatter_config_fallback = nil,
-
-        -- update imports on file move
         update_imports_on_move = true,
-        require_confirmation_on_move = true,
+        require_confirmation_on_move = false,
         watch_dir = nil,
-
-        -- filter diagnostics
         filter_out_diagnostics_by_severity = {},
         filter_out_diagnostics_by_code = {}
     }
-
-    -- required to fix code action ranges and filter diagnostics
     ts_utils.setup_client(client)
 end
 

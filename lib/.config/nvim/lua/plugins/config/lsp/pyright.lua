a9efa62954a37910a65c3a4e588a98969e82c254
Andreas Krühlmann <andreas@kruhlmann.dev>
2022-02-21 21:43:57 +0100

    Updated dotfiles

--- lib/.config/nvim/lua/plugins/config/lsp/pyright.lua
+++ lib/.config/nvim/lua/plugins/config/lsp/pyright.lua
@@ -1,9 +1,1 @@
-local lspconfig = require('lspconfig')
-local coq = require('coq')
-
-local setup = function(on_attach, capabilities)
-    lspconfig.pyright.setup(coq.lsp_ensure_capabilities())
-    lspconfig.pyright.setup({on_attach = on_attach, capabilities = capabilities })
-end
-
-return setup
+/mnt/hdd/home/doc/src/github.com/kruhlmann/dotfiles/lib/.config/nvim/lua/plugins/config/lsp/pyright.lua
--- lib/.config/nvim/lua/plugins/config/lsp/solargraph.lua
+++ lib/.config/nvim/lua/plugins/config/lsp/solargraph.lua
@@ -1,9 +1,1 @@
-local lspconfig = require('lspconfig')
-local coq = require('coq')
-
-local setup = function(on_attach, capabilities)
-    lspconfig.solargraph.setup(coq.lsp_ensure_capabilities())
-    lspconfig.solargraph.setup({on_attach = on_attach, capabilities = capabilities })
-end
-
-return setup
+/mnt/hdd/home/doc/src/github.com/kruhlmann/dotfiles/lib/.config/nvim/lua/plugins/config/lsp/solargraph.lua

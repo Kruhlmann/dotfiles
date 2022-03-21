38ec5ac00fce1f661d8d192d0a05eecb1701c26c
Andreas Krühlmann <andreas@kruhlmann.dev>
2022-03-21 11:46:53 +0100

    Updated dotfiles

--- lib/.config/nvim/lua/plugins/config/lsp/init.lua
+++ lib/.config/nvim/lua/plugins/config/lsp/init.lua
@@ -1,1 +1,62 @@
-/mnt/hdd/home/doc/src/github.com/kruhlmann/dotfiles/lib/.config/nvim/lua/plugins/config/lsp/init.lua
+vim.g.coq_settings = {
+    ["keymap.pre_select"] = true,
+    ["auto_start"] = true,
+}
+
+local protocol = require('vim.lsp.protocol')
+local saga = require("lspsaga")
+local coq = require "coq"
+
+local buf_map = function(bufnr, mode, lhs, rhs, opts)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or {
+        silent = true,
+    })
+end
+
+local on_attach = function(client, bufnr)
+    protocol.CompletionItemKind = {
+        '', 'ƒ', 'ƒ', '', '識', '𝑋', '', '', '', '',
+        '猪', '', '了', '', '﬌', '', '', '', '', '',
+        '', '', '', '', ''
+    }
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+
+    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+    vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
+    vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
+    vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
+    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+end
+
+saga.init_lsp_saga()
+local capabilities = protocol.make_client_capabilities()
+
+package.path = package.path .. ";/home/ges/doc/src/github.com/kruhlmann/dotfiles/lib/.config/nvim/lua/plugins/config/lsp/?.lua"
+require "null_ls"(on_attach, capabilities, buf_map)
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.tsserver.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyright.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.solargraph.setup{}
+require'lspconfig'.solargraph.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.solidity_ls.setup{}
+require'lspconfig'.solidity_ls.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.cmake.setup{}
+require'lspconfig'.cmake.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.bashls.setup(coq.lsp_ensure_capabilities())
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.dockerls.setup(coq.lsp_ensure_capabilities())
--- lib/.config/nvim/lua/plugins/config/lsp/null_ls.lua
+++ lib/.config/nvim/lua/plugins/config/lsp/null_ls.lua
@@ -1,1 +1,56 @@
-/mnt/hdd/home/doc/src/github.com/kruhlmann/dotfiles/lib/.config/nvim/lua/plugins/config/lsp/null_ls.lua
+local lspconfig = require("lspconfig")
+local null_ls = require("null-ls")
+
+local no_really = {
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = { "markdown", "text" },
+    generator = {
+        fn = function(params)
+            local diagnostics = {}
+            for i, line in ipairs(params.content) do
+                local col, end_col = line:find("really")
+                if col and end_col then
+                    table.insert(diagnostics, {
+                        row = i,
+                        col = col,
+                        end_col = end_col,
+                        source = "no-really",
+                        message = "Don't use 'really!'",
+                        severity = 2,
+                    })
+                end
+            end
+            return diagnostics
+        end,
+    },
+}
+
+null_ls.register(no_really)
+
+local setup = function(on_attach, capabilities, buf_map)
+    null_ls.setup({
+        sources = {
+            null_ls.builtins.formatting.black, null_ls.builtins.formatting.isort,
+            null_ls.builtins.formatting.eslint_d,
+            null_ls.builtins.formatting.asmfmt,
+            null_ls.builtins.formatting.prettier_d_slim,
+            null_ls.builtins.formatting.clang_format,
+            null_ls.builtins.formatting.cmake_format,
+            null_ls.builtins.formatting.goimports, null_ls.builtins.formatting.golines,
+            null_ls.builtins.formatting.gofumpt, null_ls.builtins.formatting.json_tool,
+            null_ls.builtins.formatting.lua_format,
+            null_ls.builtins.formatting.nginx_beautifier,
+            null_ls.builtins.formatting.rufo,
+            null_ls.builtins.formatting.shellharden,
+            null_ls.builtins.formatting.uncrustify,
+            null_ls.builtins.diagnostics.shellcheck,
+            null_ls.builtins.diagnostics.luacheck,
+            null_ls.builtins.diagnostics.write_good,
+            null_ls.builtins.diagnostics.misspell, null_ls.builtins.diagnostics.vint,
+            null_ls.builtins.completion.spell, null_ls.builtins.diagnostics.pylint,
+        },
+        on_attach = on_attach,
+    })
+end
+
+return setup
